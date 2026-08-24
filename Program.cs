a2b250f3648500b7c52ae92df7c4d37f33140b95
osama ahmed <oa9064626@gmail.com>
2026-08-24 21:27:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Assignment_5_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
