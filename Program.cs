e593b418a938d59d13eb68544c709cfa1ad937dc
osama ahmed <oa9064626@gmail.com>
2026-08-24 21:34:37 +0300

    Question 1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,57 @@
 ﻿
+using Microsoft.VisualBasic;
+using System.Numerics;
+using System.Threading.Channels;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace Assignment_5_oop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1  Abstraction
+
+
+
+            //            a)  What is Abstraction in Object - Oriented Programming?
+
+
+//            Abstraction is considered one of the four core pillars of Object - Oriented Programming(OOP) for the following reasons:
+
+//Reduces Complexity: It allows developers to break down large, complex systems into smaller, manageable components by hiding intricate background logic.
+
+//Enhances Maintainability and Code Reusability: Changes to internal implementation details can be made without affecting other parts of the application that interact with the simplified interface.
+
+//Improves Code Security: By exposing only necessary methods and hiding internal data structures, sensitive logic is protected from unintended modification or misuse.
+
+//Increases Flexibility and Scalability: It establishes a clear contract for classes through abstract interfaces, allowing new features or subclasses to be introduced seamlessly.
+
+
+
+
+
+            //b)  Why is abstraction considered one of the four pillars of OOP ?
+
+//            Abstraction is considered one of the four pillars of Object-Oriented Programming(OOP) because:
+
+//Reduces System Complexity: It allows developers to break down large systems into manageable components by hiding complex implementation details behind clean interfaces.
+
+//Enhances Code Reusability and Maintainability: Internal code logic can be updated, optimized, or refactored without breaking other parts of the application that depend on the interface.
+
+//Improves Security: It protects sensitive internal state and logic by exposing only the necessary methods for interacting with the object.
+
+//Increases Flexibility: It enforces a standard design contract(via Abstract Classes or Interfaces), allowing new classes to be added or swapped easily without changing existing code.
+
+
+
+
+
+            #endregion
+
+
+
         }
     }
 }
